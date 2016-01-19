5d1322e3716831e02f2416daae7c07a11a400f04
vntalgo <alexandre.depaula@sonymobile.com>
2016-01-19 14:40:32 -0200

    Adding add user and services listing screens

---
+++ MyService/app/src/main/java/com/myservice/view/activity/AddUserActivity.java
@@ -0,0 +1,15 @@
+package com.myservice.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.myservice.R;
+
+public class AddUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user);
+    }
+}
