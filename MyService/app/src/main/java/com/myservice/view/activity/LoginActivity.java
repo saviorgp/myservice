4090571f35349e076094193ef80c0545f9ac9021
integritas-jozecarlos <i4525001314s>
2016-01-21 00:11:43 -0200

    login labels

--- MyService/app/src/main/java/com/myservice/view/activity/LoginActivity.java
+++ MyService/app/src/main/java/com/myservice/view/activity/LoginActivity.java
@@ -1,15 +1,12 @@
 package com.myservice.view.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.myservice.R;
 
 
-/**
- * A login screen that offers login via email/password.
- */
-public class LoginActivity extends AppCompatActivity{
+public class LoginActivity extends Activity{
 
 
 
