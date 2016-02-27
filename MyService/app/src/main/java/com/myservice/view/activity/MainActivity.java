8b87aa1e12fd7a27043029df70cf7120ee3e48df
integritas-jozecarlos <i4525001314s>
2016-02-27 18:20:56 -0300

    Main Screen Toolbar

--- MyService/app/src/main/java/com/myservice/view/activity/MainActivity.java
+++ MyService/app/src/main/java/com/myservice/view/activity/MainActivity.java
@@ -1,8 +1,15 @@
 package com.myservice.view.activity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -37,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 
     @Override
