b746490a919dcb71c095a8cbed2effb4f8875700
integritas-jozecarlos <i4525001314s>
2016-03-20 22:38:32 -0300

    Fixing problems on contact view

--- MyService/app/src/main/java/com/myservice/view/activity/ContactActivity.java
+++ MyService/app/src/main/java/com/myservice/view/activity/ContactActivity.java
@@ -4,6 +4,7 @@ package com.myservice.view.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.myservice.R;
@@ -23,12 +24,22 @@ public class ContactActivity extends AppCompatActivity {
         advertisement = (Advertisement) it.getSerializableExtra("ADVERTISEMENT");
 
         if (advertisement != null) {
-            ((TextView) findViewById(R.id.txt_item_title)).setText(advertisement.getUser().getName() + " " + advertisement.getUser().getLastName());
+            String name = advertisement.getUser().getName() + " " + advertisement.getUser().getLastName();
+
+            ((TextView) findViewById(R.id.txt_item_title)).setText(name);
             ((TextView) findViewById(R.id.txt_contact_email)).setText(advertisement.getUser().getEmail());
             ((TextView) findViewById(R.id.txt_contact_phone)).setText(advertisement.getUser().getPhone());
             ((TextView) findViewById(R.id.txt_contact_whatsapp)).setText(advertisement.getUser().getCelPhone());
             ((TextView) findViewById(R.id.txt_contact_descricao)).setText(advertisement.getDescription());
             ((TextView) findViewById(R.id.txt_contact_location)).setText(advertisement.getUser().getAddress());
+            ((TextView) findViewById(R.id.txt_toolbar_user_name)).setText(name);
+
+            findViewById(R.id.menu_left_back).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    finish();
+                }
+            });
         }
     }
 }
